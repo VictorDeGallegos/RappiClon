04d2722cc8082ce8eba08f1ed46c84d9c797a5bd
VictorGallegos <316160456@ciencias.unam.mx>
2023-08-21 20:35:14 -0600

    style(Theme): Add color to light and black theme  🌈

--- app/src/main/java/com/example/rappiclon/ui/theme/Color.kt
+++ app/src/main/java/com/example/rappiclon/ui/theme/Color.kt
@@ -9,3 +9,16 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val primaryColor = HexToColor.getColor("#fa3d22")
+val whiteColor = HexToColor.getColor("#ffffff")
+val backgroundDarkColor = HexToColor.getColor("#19202c")
+val blackColor = HexToColor.getColor("#222a32")
+val surfaceDark = HexToColor.getColor("#1c2639")
+
+object HexToColor {
+    fun getColor(color: String): Color {
+        return Color(android.graphics.Color.parseColor(color))
+    }
+}
